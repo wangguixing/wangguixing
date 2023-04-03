7a592171783182a8f60a8ae9ce6c8c9db934fd03
wangguixing <1163260785@qq.com>
2023-04-03 22:39:40 +0800

    update arcro-vue状态

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,9 +1,9 @@
 /*
  * @Author: wangguixing 1163260785@qq.com
  * @Date: 2023-03-07 23:54:04
- * @LastEditors: wangguixing 1163260785@qq.com
- * @LastEditTime: 2023-03-31 21:35:08
- * @FilePath: \wangguixing\src\constants\data.js
+ * @LastEditors: wangguixing
+ * @LastEditTime: 2023-04-03 22:39:03
+ * @FilePath: \src\constants\data.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 const badges = require("./badges");
@@ -27,7 +27,7 @@ const data = {
       github: "https://github.com/wangguixing/arcro-vue",
       preview: "https://github.com/wangguixing/arcro-vue/blob/master/README.md",
       techList: [badges.Vue, , badges.TypeScript, badges.less],
-      status: "进行中",
+      status: "1.0.0",
     },
   ],
 };
