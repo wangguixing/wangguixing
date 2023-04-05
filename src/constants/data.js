f709f1c444fec92483a91140e295b28db568f1ad
wangguixing <1163260785@qq.com>
2023-04-05 23:19:27 +0800

    deploy

--- src/constants/data.js
+++ src/constants/data.js
@@ -2,7 +2,7 @@
  * @Author: wangguixing 1163260785@qq.com
  * @Date: 2023-03-07 23:54:04
  * @LastEditors: wangguixing
- * @LastEditTime: 2023-04-05 23:17:28
+ * @LastEditTime: 2023-04-05 23:19:05
  * @FilePath: \src\constants\data.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -24,7 +24,6 @@ const data = {
       repo: "🌈arcro-vue",
       desc: "vue3，vite，tsx，pinia，@arco-design-vue",
       github: "https://github.com/wangguixing/arcro-vue",
-      preview: "https://github.com/wangguixing/arcro-vue/blob/master/README.md",
       techList: [badges.Vue, , badges.TypeScript, badges.less],
       status: "1",
       remark: "新增add-node-polyfill分支",
@@ -34,8 +33,6 @@ const data = {
       repo: "vue3-babylon-ts",
       desc: "babylonjs vue3 ts demo",
       github: "https://github.com/wangguixing/vue3-babylon-ts",
-      preview:
-        "https://github.com/wangguixing/vue3-babylon-ts/blob/master/README.md",
       techList: [badges.Vue, , badges.TypeScript, badges.babylonjs],
       status: "",
       remark: "",
