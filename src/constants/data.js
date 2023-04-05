d15398d843d4ba1ec050b12529e2afeeb828256c
wangguixing <1163260785@qq.com>
2023-04-05 23:17:51 +0800

    deploy

--- src/constants/data.js
+++ src/constants/data.js
@@ -2,7 +2,7 @@
  * @Author: wangguixing 1163260785@qq.com
  * @Date: 2023-03-07 23:54:04
  * @LastEditors: wangguixing
- * @LastEditTime: 2023-04-05 23:15:08
+ * @LastEditTime: 2023-04-05 23:17:28
  * @FilePath: \src\constants\data.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -27,7 +27,7 @@ const data = {
       preview: "https://github.com/wangguixing/arcro-vue/blob/master/README.md",
       techList: [badges.Vue, , badges.TypeScript, badges.less],
       status: "1",
-      remark: "add-node-polyfill分支增加了node部分api",
+      remark: "新增add-node-polyfill分支",
     },
     {
       logo: "",
